040b87817bbcf495f963fb390127b29a7b3c77f3
Luan Vu <luan.vu@asnet.com.vn>
2025-01-15 16:29:41 +0700

    try downgrade json server version

--- src/javascripts/config.js
+++ src/javascripts/config.js
@@ -3,7 +3,7 @@ const config = {
     apiUrl: 'http://localhost:3000',
   },
   production: {
-    apiUrl: 'https://json-server-vercel-kwwfkvsdk-luan-vus-projects-c4babaef.vercel.app/',
+    apiUrl: 'https://json-server-8zouvu039-luan-vus-projects-c4babaef.vercel.app/',
   },
 };
 export const getApiUrl = () => {
