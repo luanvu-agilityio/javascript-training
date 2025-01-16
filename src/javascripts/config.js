86e0e27a038284c7920064bbaf1038978d27e2a2
Luan Vu <luan.vu@asnet.com.vn>
2025-01-16 09:37:51 +0700

    update code

--- src/javascripts/config.js
+++ src/javascripts/config.js
@@ -3,7 +3,7 @@ const config = {
     apiUrl: 'http://localhost:3000',
   },
   production: {
-    apiUrl: 'https://json-server-ecafvf72a-luan-vus-projects-c4babaef.vercel.app',
+    apiUrl: 'https://json-server-b1faln7gb-luan-vus-projects-c4babaef.vercel.app',
   },
 };
 
