c99262f44f140c9302ed85b2ab44d8b12e096f49
Luan Vu <116854422+luanvvk@users.noreply.github.com>
2025-01-20 23:42:51 +0700

    test deploy

--- src/javascripts/config.js
+++ src/javascripts/config.js
@@ -3,7 +3,7 @@ const config = {
     apiUrl: 'http://localhost:3000',
   },
   production: {
-    apiUrl: 'https://json-server-api-vercel-hwfn1957f-luan-vus-projects-c4babaef.vercel.app',
+    apiUrl: 'https://json-server-api-vercel-l7g90bar1-luan-vus-projects-c4babaef.vercel.app',
   },
 };
 
